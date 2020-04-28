fbfcb927494f570ef0d96f90e145afe8ce54078a
fuxiaohao <fuxiao4@hao>
2020-04-28 11:39:05 +0800

    fuxiao-hao

---
+++ src/main/java/com/newer/TrendshomeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.newer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendshomeHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendshomeHomeApplication.class, args);
+	}
+
+}
